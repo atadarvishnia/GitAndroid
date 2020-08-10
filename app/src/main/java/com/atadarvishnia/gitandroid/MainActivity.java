c491ae09c0279443a16b1b7470abbd782a3cacde
ata <atadarvishnya@yahoo.com>
2020-08-10 17:37:15 +0430

    MainActivity Log i onCreate tag Commit

--- app/src/main/java/com/atadarvishnia/gitandroid/MainActivity.java
+++ app/src/main/java/com/atadarvishnia/gitandroid/MainActivity.java
@@ -3,12 +3,17 @@ package com.atadarvishnia.gitandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate: ");
     }
 }
